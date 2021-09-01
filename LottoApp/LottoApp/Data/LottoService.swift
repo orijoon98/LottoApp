8efed1a37e869fd49591cc6b172122b0bfc80261
orijoon98 <orijoon98@gmail.com>
2021-09-01 13:07:39 +0900

    JSON 정보 받아오기

--- LottoApp/LottoApp/Data/LottoService.swift
+++ LottoApp/LottoApp/Data/LottoService.swift
@@ -1,5 +1,11 @@
 import UIKit
 
+enum NetworkError: Error{
+    case badURL
+    case noData
+    case decodingError
+}
+
 class LottoService {
     static let shared = LottoService()
     
@@ -7,19 +13,27 @@ class LottoService {
         
     }
     
-    func fetchLotto() {
-        if let url = URL(string: "http://112.169.11.3:30080") {
-            var request = URLRequest.init(url: url)
-            
-            request.httpMethod = "GET"
-            
-            URLSession.shared.dataTask(with: request) { (data, response, error) in
-                guard let data = data else { return }
-                if let json = try? JSONDecoder().decode(LottoData.self, from: data) {
-                    print(json.datas)
-                }
-            }.resume()
+    let url = URL(string: "http://112.169.11.3:30080")
+    
+    func fetchLotto(completion: @escaping (Result<LottoData, NetworkError>) -> Void) {
+        guard let url = url else {
+            print("badURL")
+            return completion(.failure(.badURL))
         }
+        var request = URLRequest.init(url: url)
+        request.httpMethod = "GET"
+        URLSession.shared.dataTask(with: request) {
+            data, response, error in guard let data = data, error == nil else {
+                print("noData")
+                return completion(.failure(.noData))
+            }
+            let lottoData = try? JSONDecoder().decode(LottoData.self, from: data)
+            if let lottoData = lottoData {
+                completion(.success(lottoData)) } else {
+                    print("decodingError")
+                    completion(.failure(.decodingError))
+            }
+        }.resume()
     }
 }
 
--- LottoApp/LottoApp/ViewController.swift
+++ LottoApp/LottoApp/ViewController.swift
@@ -3,13 +3,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var lottoList = Array<Dictionary<String, Any>>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        LottoService.shared.fetchLotto()
+        lottoList.removeAll()
+        updateData()
     }
     
-    
+    func updateData() {
+        LottoService.shared.fetchLotto { result in
+            switch result {
+            case .success(let lottoData): DispatchQueue.main.async {
+                for i in (0...52) {
+                    var curDict = Dictionary<String, Any>()
+                    curDict["drwNoDate"] = lottoData.datas[i].drwNoDate
+                    curDict["drwNo"] = lottoData.datas[i].drwNo
+                    curDict["drwtNo1"] = lottoData.datas[i].drwtNo1
+                    curDict["drwtNo2"] = lottoData.datas[i].drwtNo2
+                    curDict["drwtNo3"] = lottoData.datas[i].drwtNo3
+                    curDict["drwtNo4"] = lottoData.datas[i].drwtNo4
+                    curDict["drwtNo5"] = lottoData.datas[i].drwtNo5
+                    curDict["drwtNo6"] = lottoData.datas[i].drwtNo6
+                    curDict["bnusNo"] = lottoData.datas[i].bnusNo
+                    curDict["firstWinamnt"] = lottoData.datas[i].firstWinamnt
+                    curDict["firstAccumamnt"] = lottoData.datas[i].firstAccumamnt
+                    curDict["firstPrzwnerCo"] = lottoData.datas[i].firstPrzwnerCo
+                    curDict["totSellamnt"] = lottoData.datas[i].totSellamnt
+                    self.lottoList.append(curDict)
+                }
+                print(self.lottoList)
+            }
+            case .failure(_): print("error")
+            }
+        }
+    }
 }
 
